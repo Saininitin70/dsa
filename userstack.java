74257280d95281e95bb1398a8551e50c74c9cda2
Saininitin70 <nitinsain540@gmail.com>
2025-03-21 15:10:37 +0530

    Implement userstack class with push, pop, and utility methods

---
+++ userstack.java
@@ -0,0 +1,83 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class userstack
+{
+    private int top;
+    private final int maxSize;
+    private final int[] stackArray;
+    
+    public userstack(int s){
+        maxSize = s;
+        stackArray = new int[maxSize];
+        top = -1;
+    }
+    
+    public void push(int value){
+        if(top == maxSize - 1){
+            System.out.println("Stack is full");
+            return;
+        
+        }
+        
+        stackArray[++top] = value;
+        System.out.println("Pushed into the stack " + value);
+    }
+
+    
+    public int pop(){
+
+        if(top == -1){
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        int poppedelement = stackArray[top];
+        System.out.println("Popped from the stack " + poppedelement);
+        return poppedelement;
+        
+    }
+    
+    public int top(){
+        if(top == -1){
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        return stackArray[top];
+    }
+    
+    public boolean isEmpty(){
+        return (top == -1);
+    }
+    
+    public boolean isFull(){
+        return (top == maxSize - 1);
+    }
+    
+    public static void main(String[] args) throws IOException{
+        
+       
+        
+        BufferedReader Br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter the number of elements in the stack");
+        int n = Integer.parseInt(Br.readLine());
+        int[] arr = new int[n];
+        
+        userstack theStack = new userstack(n);
+        for(int i = 0; i < n; i++)
+        {
+            System.out.println("Enter the element");
+            arr[i] = Integer.parseInt(Br.readLine());
+            theStack.push(arr[i]);
+        }        
+
+        System.out.println("Top Element of the stack is " + theStack.top());
+        
+        theStack.pop();
+        theStack.pop();
+
+        System.out.println("is stack empty? " + theStack.isEmpty());
+        System.out.println("is stack full? " + theStack.isFull());
+        
+        }
+    }
